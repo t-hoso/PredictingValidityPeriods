56b3ec79161c3f0d101d5ffc809741f2920c044b
t-hoso <taishihosokawa@yahoo.co.jp>
2020-04-15 14:27:17 +0900

    add new feature: average_word_length

--- feature_extraction/feature_extraction.py
+++ feature_extraction/feature_extraction.py
@@ -5,9 +5,8 @@ def tokenize_sentences(sentences):
     return [nltk.word_tokenize(s) for s in sentences]
 
 def average_word_length(tokenized_sentences):
-    length = np.array(list(map(len, tokenized_sentence) for tokenized_sentence in tokenized_sentences))
-    print(length)
-    return length.mean(axis=1)
+    length = [np.array(list(map(len, tokenized_sentence))).mean() for tokenized_sentence in tokenized_sentences]
+    return length
     ##token_length = []
     #[list(map(len, tokenized_sentence)) for tokenized_sentence in tokenize_sentences()]
     #for tokenized_sentence in tokenized_sentences:
